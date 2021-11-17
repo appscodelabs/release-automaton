2c8864d77344e73d1a480f0a1b18c7fed1dabe3c
Tamal Saha <tamal@appscode.com>
2021-11-17 01:53:18 -0800

    KubeDB v2021.11.18
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubedb_create_release.go
+++ cmds/kubedb_create_release.go
@@ -50,7 +50,7 @@ func NewCmdKubeDBCreateRelease() *cobra.Command {
 
 func CreateKubeDBReleaseFile() api.Release {
 	prerelease := ""
-	releaseNumber := "v2021.09.30" + prerelease
+	releaseNumber := "v2021.11.18" + prerelease
 	return api.Release{
 		ProductLine:       "KubeDB",
 		Release:           releaseNumber,
@@ -59,61 +59,69 @@ func CreateKubeDBReleaseFile() api.Release {
 		Projects: []api.IndependentProjects{
 			{
 				"github.com/kubedb/apimachinery": api.Project{
-					Tag: github.String("v0.22.0" + prerelease),
+					Tag: github.String("v0.23.0" + prerelease),
 				},
 			},
 			{
 				"github.com/kubedb/cli": api.Project{
 					Key: "kubedb-cli",
-					Tag: github.String("v0.22.0" + prerelease),
+					Tag: github.String("v0.23.0" + prerelease),
 				},
 				"github.com/kubedb/elasticsearch": api.Project{
-					Tag: github.String("v0.22.0" + prerelease),
+					Tag: github.String("v0.23.0" + prerelease),
 				},
 				"github.com/kubedb/mariadb": api.Project{
-					Tag: github.String("v0.6.0" + prerelease),
+					Tag: github.String("v0.7.0" + prerelease),
 				},
 				"github.com/kubedb/memcached": api.Project{
-					Tag: github.String("v0.15.0" + prerelease),
+					Tag: github.String("v0.16.0" + prerelease),
 				},
 				"github.com/kubedb/mongodb": api.Project{
-					Tag: github.String("v0.15.0" + prerelease),
+					Tag: github.String("v0.16.0" + prerelease),
 				},
 				"github.com/kubedb/mysql": api.Project{
-					Tag: github.String("v0.15.0" + prerelease),
+					Tag: github.String("v0.16.0" + prerelease),
 				},
 				"github.com/kubedb/postgres": api.Project{
-					Tag: github.String("v0.22.0" + prerelease),
+					Tag: github.String("v0.23.0" + prerelease),
 				},
 				"github.com/kubedb/redis": api.Project{
-					Tag: github.String("v0.15.0" + prerelease),
+					Tag: github.String("v0.16.0" + prerelease),
 				},
 				"github.com/kubedb/percona-xtradb": api.Project{
-					Tag: github.String("v0.9.0" + prerelease),
+					Tag: github.String("v0.10.0" + prerelease),
 				},
 				"github.com/kubedb/pg-coordinator": api.Project{
-					Tag: github.String("v0.6.0" + prerelease),
+					Tag: github.String("v0.7.0" + prerelease),
 					// update catalog
 				},
 				"github.com/kubedb/mariadb-coordinator": api.Project{
-					Tag: github.String("v0.2.0" + prerelease),
+					Tag: github.String("v0.3.0" + prerelease),
 					// update catalog
 				},
-				"github.com/kubedb/redis-coordinator": api.Project{
+				"github.com/kubedb/mysql-coordinator": api.Project{
 					Tag: github.String("v0.1.0" + prerelease),
 					// update catalog
 				},
+				"github.com/kubedb/mysql-router-init": api.Project{
+					Tag: github.String("v0.1.0" + prerelease),
+					// update catalog
+				},
+				"github.com/kubedb/redis-coordinator": api.Project{
+					Tag: github.String("v0.2.0" + prerelease),
+					// update catalog
+				},
 				"github.com/kubedb/replication-mode-detector": api.Project{
-					Tag: github.String("v0.9.0" + prerelease),
+					Tag: github.String("v0.10.0" + prerelease),
 					// update catalog
 				},
 				"github.com/kubedb/tests": api.Project{
-					Tag: github.String("v0.7.0" + prerelease),
+					Tag: github.String("v0.8.0" + prerelease),
 				},
 			},
 			{
 				"github.com/kubedb/pgbouncer": api.Project{
-					Tag: github.String("v0.9.0" + prerelease),
+					Tag: github.String("v0.10.0" + prerelease),
 					Commands: []string{
 						"release-automaton update-vars " +
 							"--env-file=${WORKSPACE}/Makefile.env " +
@@ -122,7 +130,7 @@ func CreateKubeDBReleaseFile() api.Release {
 					},
 				},
 				"github.com/kubedb/proxysql": api.Project{
-					Tag: github.String("v0.9.0" + prerelease),
+					Tag: github.String("v0.10.0" + prerelease),
 					Commands: []string{
 						"release-automaton update-vars " +
 							"--env-file=${WORKSPACE}/Makefile.env " +
@@ -135,7 +143,7 @@ func CreateKubeDBReleaseFile() api.Release {
 			{
 				"github.com/kubedb/operator": api.Project{
 					Key: "kubedb-community",
-					Tag: github.String("v0.22.0" + prerelease),
+					Tag: github.String("v0.23.0" + prerelease),
 					ChartNames: []string{
 						"kubedb-community",
 					},
@@ -144,7 +152,7 @@ func CreateKubeDBReleaseFile() api.Release {
 			{
 				"github.com/appscode/kubedb-enterprise": api.Project{
 					Key: "kubedb-enterprise",
-					Tag: github.String("v0.9.0" + prerelease),
+					Tag: github.String("v0.10.0" + prerelease),
 					ChartNames: []string{
 						"kubedb-enterprise",
 					},
@@ -153,7 +161,7 @@ func CreateKubeDBReleaseFile() api.Release {
 			{
 				"github.com/appscode/kubedb-autoscaler": api.Project{
 					Key: "kubedb-autoscaler",
-					Tag: github.String("v0.7.0" + prerelease),
+					Tag: github.String("v0.8.0" + prerelease),
 					ChartNames: []string{
 						"kubedb-autoscaler",
 					},
@@ -173,9 +181,11 @@ func CreateKubeDBReleaseFile() api.Release {
 					Commands: []string{
 						"./hack/scripts/import-crds.sh",
 						"go run ./hack/fmt/main.go --update-spec=spec.replicationModeDetector.image=kubedb/replication-mode-detector:${KUBEDB_REPLICATION_MODE_DETECTOR_TAG}",
+						"go run ./hack/fmt/main.go --kind=MariaDBVersion --update-spec=spec.coordinator.image=kubedb/mariadb-coordinator:${KUBEDB_MARIADB_COORDINATOR_TAG}",
+						"go run ./hack/fmt/main.go --kind=MySQLVersion --update-spec=spec.coordinator.image=kubedb/mysql-coordinator:${KUBEDB_MYSQL_COORDINATOR_TAG}",
+						"go run ./hack/fmt/main.go --kind=MySQLVersion --update-spec=spec.routerInitContainer.image=kubedb/kubedb/mysql-router-init:${KUBEDB_MYSQL_ROUTER_INIT_TAG}",
 						"go run ./hack/fmt/main.go --kind=PostgresVersion --update-spec=spec.coordinator.image=kubedb/pg-coordinator:${KUBEDB_PG_COORDINATOR_TAG}",
-						"go run ./hack/fmt/main.go --kind=MariaDBVersion  --update-spec=spec.coordinator.image=kubedb/mariadb-coordinator:${KUBEDB_MARIADB_COORDINATOR_TAG}",
-						"go run ./hack/fmt/main.go --kind=RedisVersion  --update-spec=spec.coordinator.image=kubedb/redis-coordinator:${KUBEDB_REDIS_COORDINATOR_TAG}",
+						"go run ./hack/fmt/main.go --kind=RedisVersion --update-spec=spec.coordinator.image=kubedb/redis-coordinator:${KUBEDB_REDIS_COORDINATOR_TAG}",
 						"make update-charts CHART_VERSION=${RELEASE} CHART_REGISTRY=${CHART_REGISTRY} CHART_REGISTRY_URL=${CHART_REGISTRY_URL}",
 						"make chart-kubedb-community CHART_VERSION=${KUBEDB_OPERATOR_TAG} CHART_REGISTRY=${CHART_REGISTRY} CHART_REGISTRY_URL=${CHART_REGISTRY_URL}",
 						"make chart-kubedb-enterprise CHART_VERSION=${APPSCODE_KUBEDB_ENTERPRISE_TAG} CHART_REGISTRY=${CHART_REGISTRY} CHART_REGISTRY_URL=${CHART_REGISTRY_URL}",
