906718f9a8bfd5e5f6cf22f26552a8ca38c4d23d
Tamal Saha <tamal@appscode.com>
2022-04-13 20:01:00 -0700

    Voyager v2022.04.13
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/voyager_create_release.go
+++ cmds/voyager_create_release.go
@@ -50,7 +50,7 @@ func NewCmdVoyagerCreateRelease() *cobra.Command {
 
 func CreateVoyagerReleaseFile() api.Release {
 	prerelease := ""
-	releaseNumber := "v2022.03.17" + prerelease
+	releaseNumber := "v2022.04.13" + prerelease
 	return api.Release{
 		ProductLine:       "Voyager",
 		Release:           releaseNumber,
@@ -59,16 +59,16 @@ func CreateVoyagerReleaseFile() api.Release {
 		Projects: []api.IndependentProjects{
 			{
 				"github.com/voyagermesh/apimachinery": api.Project{
-					Tag: github.String("v0.4.1" + prerelease),
+					Tag: github.String("v0.4.2" + prerelease),
 				},
 			},
 			{
 				"github.com/voyagermesh/haproxy-ingress": api.Project{
-					Tag: github.String("v14.2.1" + prerelease),
+					Tag: github.String("v14.2.2" + prerelease),
 				},
 				"github.com/voyagermesh/cli": api.Project{
 					Key: "voyager-cli",
-					Tag: github.String("v0.0.7" + prerelease),
+					Tag: github.String("v0.0.8" + prerelease),
 				},
 			},
 			{
