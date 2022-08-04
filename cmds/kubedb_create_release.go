c6e9332b7d8945adf54b6d1c5d790e10e5809de9
Tamal Saha <tamal@appscode.com>
2022-08-04 11:41:22 -0700

    KubeDB v2022.08.04-rc.1
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubedb_create_release.go
+++ cmds/kubedb_create_release.go
@@ -49,8 +49,8 @@ func NewCmdKubeDBCreateRelease() *cobra.Command {
 }
 
 func CreateKubeDBReleaseFile() api.Release {
-	prerelease := "-rc.0"
-	releaseNumber := "v2022.08.02" + prerelease
+	prerelease := "-rc.1"
+	releaseNumber := "v2022.08.04" + prerelease
 	return api.Release{
 		ProductLine:       "KubeDB",
 		Release:           releaseNumber,
