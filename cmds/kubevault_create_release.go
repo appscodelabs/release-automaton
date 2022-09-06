db3e8ab4c8e2e3588f95fc3b3072fea8fd3e752a
Tamal Saha <tamal@appscode.com>
2022-09-05 21:48:05 -0700

    KubeVault v2022.09.05-rc.0
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubevault_create_release.go
+++ cmds/kubevault_create_release.go
@@ -49,8 +49,8 @@ func NewCmdKubeVaultCreateRelease() *cobra.Command {
 }
 
 func CreateKubeVaultReleaseFile() api.Release {
-	prerelease := ""
-	releaseNumber := "v2022.06.16" + prerelease
+	prerelease := "-rc.0"
+	releaseNumber := "v2022.09.05" + prerelease
 	return api.Release{
 		ProductLine:       "KubeVault",
 		Release:           releaseNumber,
@@ -59,11 +59,11 @@ func CreateKubeVaultReleaseFile() api.Release {
 		Projects: []api.IndependentProjects{
 			{
 				"github.com/kubevault/apimachinery": api.Project{
-					Tag: github.String("v0.8.0" + prerelease),
+					Tag: github.String("v0.9.0" + prerelease),
 				},
 				"github.com/kubevault/unsealer": api.Project{
 					Key: "kubevault-unsealer",
-					Tag: github.String("v0.8.0" + prerelease),
+					Tag: github.String("v0.9.0" + prerelease),
 				},
 			},
 			{
@@ -72,11 +72,11 @@ func CreateKubeVaultReleaseFile() api.Release {
 					ChartNames: []string{
 						"kubevault-operator",
 					},
-					Tag: github.String("v0.8.0" + prerelease),
+					Tag: github.String("v0.9.0" + prerelease),
 				},
 				"github.com/kubevault/cli": api.Project{
 					Key: "kubevault-cli",
-					Tag: github.String("v0.8.0" + prerelease),
+					Tag: github.String("v0.9.0" + prerelease),
 				},
 				// {
 				// 	"github.com/kubevault/prometheus-exporter": api.Project{
