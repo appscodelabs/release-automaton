c872d92a50ea3a842804edf1626036bec23e13dc
Tamal Saha <tamal@appscode.com>
2022-12-24 09:08:00 +0600

    KubeDB v2022.12.24-rc.1
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubedb_create_release.go
+++ cmds/kubedb_create_release.go
@@ -49,8 +49,8 @@ func NewCmdKubeDBCreateRelease() *cobra.Command {
 }
 
 func CreateKubeDBReleaseFile() api.Release {
-	prerelease := "-rc.0"
-	releaseNumber := "v2022.12.13" + prerelease
+	prerelease := "-rc.1"
+	releaseNumber := "v2022.12.24" + prerelease
 	return api.Release{
 		ProductLine:       "KubeDB",
 		Release:           releaseNumber,
