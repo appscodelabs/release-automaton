ae1d202b06bba8a4934c1e9c0d025c691b3cfd6f
Tamal Saha <tamal@appscode.com>
2025-02-18 10:50:12 -0800

    KubeDB v2025.2.19
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubedb_create_release.go
+++ cmds/kubedb_create_release.go
@@ -49,8 +49,8 @@ func NewCmdKubeDBCreateRelease() *cobra.Command {
 }
 
 func CreateKubeDBReleaseFile() api.Release {
-	prerelease := "-rc.0"
-	releaseNumber := "v2025.2.6" + prerelease
+	prerelease := ""
+	releaseNumber := "v2025.2.19" + prerelease
 	return api.Release{
 		ProductLine:       "KubeDB",
 		Release:           releaseNumber,
