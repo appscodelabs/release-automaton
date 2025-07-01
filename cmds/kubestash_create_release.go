e6224fb4ee76a5f4844836cb2df5a8b879ca183b
Anisur Rahman <anisur@appscode.com>
2025-07-01 15:48:56 +0600

    Add function:kubedump-restore file
    Signed-off-by: Anisur Rahman <anisur@appscode.com>

--- cmds/kubestash_create_release.go
+++ cmds/kubestash_create_release.go
@@ -85,6 +85,7 @@ func CreateKubeStashReleaseFile() api.Release {
 						"./hack/scripts/import-crds.sh",
 
 						"go run ./hack/fmt/main.go --kind=Function --name=kubedump-backup --update-spec=spec.image=ghcr.io/kubestash/kubedump:${KUBESTASH_KUBEDUMP_TAG}",
+						"go run ./hack/fmt/main.go --kind=Function --name=kubedump-restore --update-spec=spec.image=ghcr.io/kubestash/kubedump:${KUBESTASH_KUBEDUMP_TAG}",
 						"go run ./hack/fmt/main.go --kind=Function --name=manifest-backup --update-spec=spec.image=ghcr.io/kubestash/manifest:${KUBESTASH_MANIFEST_TAG}",
 						"go run ./hack/fmt/main.go --kind=Function --name=manifest-restore --update-spec=spec.image=ghcr.io/kubestash/manifest:${KUBESTASH_MANIFEST_TAG}",
 						"go run ./hack/fmt/main.go --kind=Function --name=pvc-backup --update-spec=spec.image=ghcr.io/kubestash/pvc:${KUBESTASH_PVC_TAG}",
