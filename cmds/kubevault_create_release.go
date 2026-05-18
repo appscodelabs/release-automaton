2b0f1fc4573b4c0dcc53983ff9aef921e331a73d
Tamal Saha <tamal@appscode.com>
2026-05-18 18:05:02 +0600

    KubeVault v2026.5.18-rc.1
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubevault_create_release.go
+++ cmds/kubevault_create_release.go
@@ -49,8 +49,8 @@ func NewCmdKubeVaultCreateRelease() *cobra.Command {
 }
 
 func CreateKubeVaultReleaseFile() api.Release {
-	prerelease := "-rc.0"
-	releaseNumber := "v2026.5.14" + prerelease
+	prerelease := "-rc.1"
+	releaseNumber := "v2026.5.18" + prerelease
 	return api.Release{
 		ProductLine:       "KubeVault",
 		Release:           releaseNumber,
