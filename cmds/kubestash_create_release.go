4092e0f9e758077ee2c916929f59c7a502227437
Tamal Saha <tamal@appscode.com>
2026-06-19 14:55:57 +0530

    KubeStash v2026.6.19
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubestash_create_release.go
+++ cmds/kubestash_create_release.go
@@ -49,8 +49,8 @@ func NewCmdKubeStashCreateRelease() *cobra.Command {
 }
 
 func CreateKubeStashReleaseFile() api.Release {
-	prerelease := "-rc.2"
-	releaseNumber := "v2026.6.18" + prerelease
+	prerelease := ""
+	releaseNumber := "v2026.6.19" + prerelease
 	return api.Release{
 		ProductLine:       "KubeStash",
 		Release:           releaseNumber,
