98a8ab20667c75922d78ca3c6653532c1195be84
Tamal Saha <tamal@appscode.com>
2026-08-26 00:56:24 +0600

    KubeDB v2026.8.26-rc.2
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/kubedb_create_release.go
+++ cmds/kubedb_create_release.go
@@ -49,8 +49,8 @@ func NewCmdKubeDBCreateRelease() *cobra.Command {
 }
 
 func CreateKubeDBReleaseFile() api.Release {
-	prerelease := "-rc.1"
-	releaseNumber := "v2026.8.25" + prerelease
+	prerelease := "-rc.2"
+	releaseNumber := "v2026.8.26" + prerelease
 	// hideDocs hides this release's docs from the website. When set, the release
 	// is not advertised as the website's operator version either.
 	hideDocs := true
